d91055aa365fb0d0ccbf72e70f4fbfac0961b373
Rusyaidi <al244098@gmail.com>
2024-01-01 23:27:56 +0800

    fix: Broken macro replacer. feat: Added alternate greetings.

--- constants/Utils.ts
+++ constants/Utils.ts
@@ -1,4 +1,7 @@
-export const humanizedISO8601DateTime = (date: string = '') => {
+import { Global } from "./GlobalValues";
+import { mmkv } from "./mmkv";
+
+export const humanizedISO8601DateTime = (date = '') => {
     let baseDate = typeof date === 'number' ? new Date(date) : new Date();
     let humanYear = baseDate.getFullYear();
     let humanMonth = (baseDate.getMonth() + 1);
@@ -9,4 +12,20 @@ export const humanizedISO8601DateTime = (date: string = '') => {
     let humanMillisecond = (baseDate.getMilliseconds() < 10 ? '0' : '') + baseDate.getMilliseconds();
     let HumanizedDateTime = (humanYear + "-" + humanMonth + "-" + humanDate + " @" + humanHour + "h " + humanMinute + "m " + humanSecond + "s " + humanMillisecond + "ms");
     return HumanizedDateTime;
+}
+
+type Rule = {
+    macro : string,
+    value : string,
+}
+
+export const replaceMacros = (text : string) => {
+    if(text == undefined) return ''
+    let newtext : string = text
+    const charName = mmkv.getString(Global.CurrentCharacter)
+    const userName = mmkv.getString(Global.CurrentUser)
+    const rules : Array<Rule> = [{macro: '{{user}}', value: userName ?? ''}, {macro: '{{char}}', value: charName ?? ''}]
+    for(const rule of rules) 
+        newtext = newtext.replaceAll(rule.macro, rule.value)
+    return newtext
 }
