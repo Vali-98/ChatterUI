062ccd3f4782b5af86b8eb8c983e52e965c8186e
Rusyaidi <al244098@gmail.com>
2024-08-01 11:54:02 +0800

    refactor: separated more CharacterList components

--- app/components/CharacterMenu/CharacterList.tsx
+++ app/components/CharacterMenu/CharacterList.tsx
@@ -1,14 +1,13 @@
-import TextBoxModal from '@components/TextBoxModal'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
-import { Characters, Chats, Logger, Style } from '@globals'
+import { Characters, Logger } from '@globals'
 import { useRouter, Stack, usePathname } from 'expo-router'
 import { useEffect, useState } from 'react'
-import { SafeAreaView, TouchableOpacity, ScrollView, Text, StyleSheet, View } from 'react-native'
+import { SafeAreaView, ScrollView } from 'react-native'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import { runOnJS } from 'react-native-reanimated'
-import { useShallow } from 'zustand/react/shallow'
 
 import CharacterListing from './CharacterListing'
+import CharacterNewMenu from './CharacterNewMenu'
+import CharactersEmpty from './CharactersEmpty'
 
 type CharInfo = {
     name: string
@@ -19,17 +18,8 @@ type CharInfo = {
 
 const CharacterList = () => {
     const router = useRouter()
-    const { setCurrentCard } = Characters.useCharacterCard(
-        useShallow((state) => ({
-            setCurrentCard: state.setCard,
-            id: state.id,
-        }))
-    )
-    const loadChat = Chats.useChat((state) => state.load)
 
     const [characterList, setCharacterList] = useState<CharInfo[]>([])
-    const [showNewChar, setShowNewChar] = useState<boolean>(false)
-    const [showDownload, setShowDownload] = useState(false)
     const [nowLoading, setNowLoading] = useState(false)
 
     const goBack = () => router.back()
@@ -48,42 +38,6 @@ const CharacterList = () => {
             Logger.log(`Could not retrieve characters.\n${error}`, true)
         }
     }
-    const setCurrentCharacter = async (charId: number, edit: boolean = false) => {
-        if (nowLoading) return
-
-        try {
-            await setCurrentCard(charId)
-            setNowLoading(true)
-            const returnedChatId = await Chats.db.query.chatNewest(charId)
-            let chatId = returnedChatId
-            if (!chatId) {
-                chatId = await Chats.db.mutate.createChat(charId)
-            }
-            if (!chatId) {
-                Logger.log('Chat creation backup has failed! Please report.', true)
-                return
-            }
-
-            await loadChat(chatId)
-
-            setNowLoading(false)
-            if (edit) router.push('/CharInfo')
-            else router.back()
-        } catch (error) {
-            Logger.log(`Couldn't load character: ${error}`, true)
-            setNowLoading(false)
-        }
-    }
-
-    const handleCreateCharacter = async (text: string) => {
-        if (!text) {
-            Logger.log('Name Cannot Be Empty!', true)
-            return
-        }
-        Characters.db.mutate.createCard(text).then(async (id) => {
-            await setCurrentCharacter(id, true)
-        })
-    }
 
     useEffect(() => {
         getCharacterList()
@@ -91,88 +45,21 @@ const CharacterList = () => {
 
     return (
         <GestureDetector gesture={gesture}>
-            <SafeAreaView style={styles.mainContainer}>
+            <SafeAreaView style={{ paddingVertical: 16, paddingHorizontal: 8, flex: 1 }}>
                 <Stack.Screen
                     options={{
                         title: 'Characters',
                         headerRight: () => (
-                            <View style={styles.headerButtonContainer}>
-                                <TouchableOpacity
-                                    style={styles.headerButtonRight}
-                                    onPress={async () => {
-                                        setShowDownload(true)
-                                    }}>
-                                    <FontAwesome
-                                        name="cloud-download"
-                                        size={28}
-                                        color={Style.getColor('primary-text1')}
-                                    />
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    style={styles.headerButtonRight}
-                                    onPress={() =>
-                                        Characters.importCharacterFromImage().then(async () => {
-                                            getCharacterList()
-                                        })
-                                    }>
-                                    <FontAwesome
-                                        name="upload"
-                                        size={28}
-                                        color={Style.getColor('primary-text1')}
-                                    />
-                                </TouchableOpacity>
-
-                                <TouchableOpacity
-                                    style={styles.headerButtonRight}
-                                    onPress={async () => {
-                                        setShowNewChar(true)
-                                    }}>
-                                    <FontAwesome
-                                        name="pencil"
-                                        size={28}
-                                        color={Style.getColor('primary-text1')}
-                                    />
-                                </TouchableOpacity>
-                            </View>
+                            <CharacterNewMenu
+                                nowLoading={nowLoading}
+                                setNowLoading={setNowLoading}
+                                getCharacterList={getCharacterList}
+                            />
                         ),
                     }}
                 />
 
-                <TextBoxModal
-                    booleans={[showNewChar, setShowNewChar]}
-                    title="[Creating Character]  Enter Name Below"
-                    onConfirm={handleCreateCharacter}
-                />
-
-                <TextBoxModal
-                    title="Enter Character Hub or Pygmalion Link"
-                    booleans={[showDownload, setShowDownload]}
-                    onConfirm={(text) =>
-                        Characters.importCharacterFromRemote(text).then(() => {
-                            getCharacterList()
-                        })
-                    }
-                    showPaste
-                />
-                {characterList.length === 0 && (
-                    <View style={{ ...styles.mainContainer, alignItems: 'center', marginTop: 30 }}>
-                        <Ionicons
-                            name="person-outline"
-                            color={Style.getColor('primary-text2')}
-                            size={60}
-                        />
-                        <Text
-                            style={{
-                                color: Style.getColor('primary-text2'),
-                                marginTop: 16,
-                                fontStyle: 'italic',
-                                fontSize: 16,
-                            }}>
-                            No Characters Found. Try Importing Some!
-                        </Text>
-                    </View>
-                )}
+                {characterList.length === 0 && <CharactersEmpty />}
 
                 {characterList.length !== 0 && (
                     <ScrollView>
@@ -193,20 +80,3 @@ const CharacterList = () => {
 }
 
 export default CharacterList
-
-const styles = StyleSheet.create({
-    mainContainer: {
-        paddingVertical: 16,
-        paddingHorizontal: 8,
-        flex: 1,
-    },
-
-    headerButtonRight: {
-        marginLeft: 20,
-        marginRight: 4,
-    },
-
-    headerButtonContainer: {
-        flexDirection: 'row',
-    },
-})
