84e0033fcaecd73f8a246fbeecd54d1990569168
Vali98 <al244098@gmail.com>
2024-08-07 10:02:46 +0800

    feat: added debug prints to logger

--- constants/Logger.ts
+++ constants/Logger.ts
@@ -10,18 +10,25 @@ export namespace Logger {
         return JSON.parse(mmkv.getString(Global.Logs) ?? '[]')
     }
 
-    export const log = (data: string, toast: boolean = false, toastTime: number = 2000) => {
-        const timestamped = `[${new Date().toTimeString().substring(0, 8)}] : ${data}`
-        console.log(timestamped)
+    const insertToLogs = (data: string) => {
         let logs = getLogs()
-        logs.push(timestamped)
-        if (toast) Toast.show(data, toastTime)
+        logs.push(data)
         if (logs.length > maxloglength) logs.shift()
         mmkv.set(Global.Logs, JSON.stringify(logs))
     }
 
+    export const log = (data: string, toast: boolean = false, toastTime: number = 2000) => {
+        const timestamped = `[${new Date().toTimeString().substring(0, 8)}] : ${data}`
+        console.log(timestamped)
+        insertToLogs(timestamped)
+        if (toast) Toast.show(data, toastTime)
+    }
+
     export const debug = (data: string) => {
-        if (__DEV__ || mmkv.getBoolean(AppSettings.DevMode)) console.log(`[Debug]: `, data)
+        if (__DEV__ || mmkv.getBoolean(AppSettings.DevMode)) {
+            insertToLogs(data)
+            console.log(`[Debug]: `, data)
+        }
     }
 
     export const flushLogs = () => {
