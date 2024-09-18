e23cd2f57281a649b3702b3709087fed7e0dd733
Vali98 <al244098@gmail.com>
2024-09-18 17:20:52 +0800

    chore: updated drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'drizzle-kit'
+import type { Config } from 'drizzle-kit'
 
-export default defineConfig({
+export default {
     schema: './db/schema.ts',
     out: './db/migrations',
+    dialect: 'sqlite',
     driver: 'expo',
-})
+} satisfies Config
