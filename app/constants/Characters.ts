8afde1bd65bbac663df2d56636f512d3a5d415d8
Vali98 <al244098@gmail.com>
2024-09-22 12:07:07 +0800

    feat: added weekday, date and time macros

--- app/constants/Characters.ts
+++ app/constants/Characters.ts
@@ -671,19 +671,25 @@ const TavernCardV2 = (name: string) => {
     }
 }
 
-type Rule = {
+type Macro = {
     macro: string
     value: string
 }
+
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 export const replaceMacros = (text: string) => {
     if (text === undefined) return ''
     let newtext: string = text
     const charName = Characters.useCharacterCard.getState().card?.data.name ?? ''
     const userName = Characters.useUserCard.getState().card?.data.name ?? ''
-    const rules: Rule[] = [
+    const time = new Date()
+    const rules: Macro[] = [
         { macro: '{{user}}', value: userName },
         { macro: '{{char}}', value: charName },
+        { macro: '{{time}}', value: time.toLocaleTimeString() },
+        { macro: '{{date}}', value: time.toLocaleDateString() },
+        { macro: '{{day}}', value: weekday[time.getDay()] },
     ]
     for (const rule of rules) newtext = newtext.replaceAll(rule.macro, rule.value)
     return newtext
