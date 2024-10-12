cc8e11483183aacbd2f08f9852b2eb1c92fdc1c3
Vali98 <al244098@gmail.com>
2024-10-12 17:29:39 +0800

    feat: migrated ChatEditPopup to use generic PopupMenu

--- app/components/ChatMenu/ChatEditPopup.tsx
+++ app/components/ChatMenu/ChatEditPopup.tsx
@@ -1,19 +1,9 @@
+import PopupMenu from '@components/PopupMenu'
 import TextBoxModal from '@components/TextBoxModal'
-import { AntDesign, FontAwesome } from '@expo/vector-icons'
-import { Characters, Chats, Logger, saveStringToDownload, Style } from '@globals'
-import { useFocusEffect } from 'expo-router'
-import React, { useRef, useState } from 'react'
-import { StyleSheet, TouchableOpacity, Text, BackHandler, Alert } from 'react-native'
-import {
-    Menu,
-    MenuOption,
-    MenuOptions,
-    MenuOptionsCustomStyle,
-    MenuTrigger,
-    renderers,
-} from 'react-native-popup-menu'
-
-const { Popover } = renderers
+import { Characters, Chats, Logger, saveStringToDownload } from '@globals'
+import React, { useState } from 'react'
+import { Alert, View } from 'react-native'
+import { Menu } from 'react-native-popup-menu'
 
 type ListItem = {
     id: number
@@ -30,35 +20,8 @@ type ChatEditPopupProps = {
     setNowLoading: (b: boolean) => void
 }
 
-type PopupProps = {
-    onPress: () => void | Promise<void>
-    label: string
-    iconName: keyof typeof FontAwesome.glyphMap
-    warning?: boolean
-}
-
-const PopupOption: React.FC<PopupProps> = ({ onPress, label, iconName, warning = false }) => {
-    return (
-        <MenuOption>
-            <TouchableOpacity style={styles.popupButton} onPress={onPress}>
-                <FontAwesome
-                    style={{ minWidth: 20 }}
-                    name={iconName}
-                    size={18}
-                    color={Style.getColor(warning ? 'destructive-brand' : 'primary-text2')}
-                />
-                <Text style={warning ? styles.optionLabelWarning : styles.optionLabel}>
-                    {label}
-                </Text>
-            </TouchableOpacity>
-        </MenuOption>
-    )
-}
-
 const ChatEditPopup: React.FC<ChatEditPopupProps> = ({ item, setNowLoading, nowLoading }) => {
-    const [showMenu, setShowMenu] = useState<boolean>(false)
     const [showRename, setShowRename] = useState<boolean>(false)
-    const menuRef: React.MutableRefObject<Menu | null> = useRef(null)
 
     const { charName, charId } = Characters.useCharacterCard((state) => ({
         charId: state.id,
@@ -72,7 +35,7 @@ const ChatEditPopup: React.FC<ChatEditPopupProps> = ({ item, setNowLoading, nowL
         unloadChat: state.reset,
     }))
 
-    const handleDeleteChat = () => {
+    const handleDeleteChat = (menuRef: React.MutableRefObject<Menu | null>) => {
         Alert.alert(
             `Delete Character`,
             `Are you sure you want to delete '${item.name}'? This cannot be undone.`,
@@ -101,7 +64,7 @@ const ChatEditPopup: React.FC<ChatEditPopupProps> = ({ item, setNowLoading, nowL
         )
     }
 
-    const handleCloneChat = () => {
+    const handleCloneChat = (menuRef: React.MutableRefObject<Menu | null>) => {
         Alert.alert(
             `Clone Character`,
             `Are you sure you want to clone '${item.name}'?`,
@@ -120,129 +83,58 @@ const ChatEditPopup: React.FC<ChatEditPopupProps> = ({ item, setNowLoading, nowL
         )
     }
 
-    const handleExportChat = async () => {
+    const handleExportChat = async (menuRef: React.MutableRefObject<Menu | null>) => {
         const name = `Chatlogs-${charName}-${item.id}`.replaceAll(' ', '_')
-        saveStringToDownload(JSON.stringify(await Chats.db.query.chat(item.id)), name, 'utf8')
+        await saveStringToDownload(JSON.stringify(await Chats.db.query.chat(item.id)), name, 'utf8')
+        menuRef.current?.close()
         Logger.log(`File: ${name} saved to downloads!`, true)
-        menuRef.current?.close()
     }
-
-    const backAction = () => {
-        if (!menuRef.current || !menuRef.current?.isOpen()) return false
-        menuRef.current?.close()
-        return true
-    }
-
-    useFocusEffect(() => {
-        BackHandler.removeEventListener('hardwareBackPress', backAction)
-        const handler = BackHandler.addEventListener('hardwareBackPress', backAction)
-        return () => handler.remove()
-    })
 
     return (
-        <Menu
-            ref={menuRef}
-            onOpen={() => setShowMenu(true)}
-            onClose={() => setShowMenu(false)}
-            renderer={Popover}
-            rendererProps={{
-                placement: 'left',
-                anchorStyle: styles.anchor,
-                openAnimationDuration: 150,
-                closeAnimationDuration: 0,
-            }}>
-            <MenuTrigger disabled={nowLoading}>
-                <AntDesign
-                    style={styles.triggerButton}
-                    color={Style.getColor(showMenu ? 'primary-text3' : 'primary-text2')}
-                    name="edit"
-                    size={26}
-                />
-            </MenuTrigger>
-            <MenuOptions customStyles={menustyle}>
-                <TextBoxModal
-                    booleans={[showRename, setShowRename]}
-                    onConfirm={async (text) => {
-                        await Chats.db.mutate.renameChat(item.id, text)
-                    }}
-                    onClose={() => menuRef.current?.close()}
-                    textCheck={(text) => text.length === 0}
-                />
-                <PopupOption
-                    onPress={() => {
-                        setShowRename(true)
-                    }}
-                    label="Rename"
-                    iconName="pencil"
-                />
-
-                <PopupOption
-                    onPress={() => handleExportChat()}
-                    label="Export"
-                    iconName="download"
-                />
-                <PopupOption
-                    onPress={() => {
-                        handleCloneChat()
-                    }}
-                    label="Clone"
-                    iconName="copy"
-                />
-                <PopupOption
-                    onPress={() => handleDeleteChat()}
-                    label="Delete"
-                    iconName="trash"
-                    warning
-                />
-            </MenuOptions>
-        </Menu>
+        <View>
+            <TextBoxModal
+                booleans={[showRename, setShowRename]}
+                onConfirm={async (text) => {
+                    await Chats.db.mutate.renameChat(item.id, text)
+                }}
+                textCheck={(text) => text.length === 0}
+            />
+            <PopupMenu
+                icon="edit"
+                disabled={nowLoading}
+                options={[
+                    {
+                        label: 'Rename',
+                        icon: 'edit',
+                        onPress: (menuRef) => {
+                            setShowRename(true)
+                            menuRef.current?.close()
+                        },
+                        closeOnExit: true,
+                    },
+                    {
+                        label: 'Export',
+                        icon: 'download',
+                        onPress: (menuRef) => handleExportChat(menuRef),
+                        closeOnExit: true,
+                    },
+                    {
+                        label: 'Clone',
+                        icon: 'copy1',
+                        onPress: handleCloneChat,
+                        closeOnExit: true,
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'delete',
+                        warning: true,
+                        onPress: handleDeleteChat,
+                        closeOnExit: true,
+                    },
+                ]}
+            />
+        </View>
     )
 }
 
 export default ChatEditPopup
-
-const styles = StyleSheet.create({
-    anchor: {
-        backgroundColor: Style.getColor('primary-surface3'),
-        padding: 4,
-    },
-
-    popupButton: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        columnGap: 12,
-        paddingVertical: 12,
-        paddingRight: 32,
-        paddingLeft: 12,
-        borderRadius: 12,
-    },
-
-    headerButtonContainer: {
-        flexDirection: 'row',
-    },
-
-    optionLabel: {
-        color: Style.getColor('primary-text1'),
-    },
-
-    optionLabelWarning: {
-        fontWeight: '500',
-        color: '#d2574b',
-    },
-
-    triggerButton: {
-        paddingHorizontal: 12,
-        paddingVertical: 20,
-    },
-})
-
-const menustyle: MenuOptionsCustomStyle = {
-    optionsContainer: {
-        backgroundColor: Style.getColor('primary-surface3'),
-        padding: 4,
-        borderRadius: 12,
-    },
-    optionsWrapper: {
-        backgroundColor: Style.getColor('primary-surface3'),
-    },
-}
