6bf9b68b8f7e7b5b8a428ba2c369a0b37a05325c
Vali98 <al244098@gmail.com>
2024-11-28 22:38:36 +0800

    feat: added ButtonPrimary as generic component

---
+++ app/components/General/ButtonPrimary.tsx
@@ -0,0 +1,82 @@
+import { Style } from '@globals'
+import {
+    Text,
+    PressableProps,
+    TextStyle,
+    Pressable,
+    ViewStyle,
+    StyleSheet,
+    Animated,
+    useAnimatedValue,
+} from 'react-native'
+
+interface ButtonPrimaryProps extends Omit<PressableProps, 'style'> {
+    labelStyle?: TextStyle
+    label?: string
+    pressStyle?: ViewStyle
+    unpressStyle?: ViewStyle
+    opacity?: number
+}
+
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
+
+const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
+    labelStyle,
+    label,
+    pressStyle,
+    unpressStyle,
+    children,
+    onPressIn,
+    opacity = 1,
+    onPressOut,
+    ...rest
+}) => {
+    const animOpacity = useAnimatedValue(1)
+
+    const handlePressIn = () => {
+        animOpacity.setValue(0.4)
+    }
+
+    const handlePressOut = () => {
+        Animated.timing(animOpacity, {
+            toValue: 1,
+            duration: 50,
+            useNativeDriver: true,
+        }).start()
+    }
+
+    return (
+        <AnimatedPressable
+            onPressIn={(event) => {
+                handlePressIn()
+                if (onPressIn) onPressIn(event)
+            }}
+            onPressOut={(event) => {
+                handlePressOut()
+                if (onPressOut) onPressOut(event)
+            }}
+            {...rest}
+            style={StyleSheet.flatten([
+                pressStyle,
+                {
+                    backgroundColor: Style.getColor('primary-brand'),
+                    padding: 8,
+                    borderRadius: 8,
+                    opacity: animOpacity,
+                    transform: [{ scaleX: 0.99 }],
+                },
+            ])}>
+            {label && (
+                <Text
+                    style={[
+                        labelStyle,
+                        { textAlign: 'center', color: Style.getColor('primary-text1') },
+                    ]}>
+                    {label}
+                </Text>
+            )}
+        </AnimatedPressable>
+    )
+}
+
+export default ButtonPrimary
