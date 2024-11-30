62863fc9320d344fda359efcda0ca4e3994a6d9e
Vali98 <al244098@gmail.com>
2024-11-30 14:25:23 +0800

    feat: added new Logger apis

--- constants/Logger.ts
+++ constants/Logger.ts
@@ -4,7 +4,29 @@ import { AppSettings, Global } from './GlobalValues'
 import { mmkv } from './MMKV'
 
 export namespace Logger {
-    const maxloglength = 100
+    const toastTime = 2000
+
+    export enum LogLevel {
+        INFO,
+        WARN,
+        ERROR,
+        DEBUG,
+    }
+
+    const LevelName: Record<LogLevel, string> = {
+        [LogLevel.INFO]: '[INFO]',
+        [LogLevel.WARN]: '[WARN]',
+        [LogLevel.ERROR]: '[ERROR]',
+        [LogLevel.DEBUG]: '[DEBUG]',
+    }
+
+    type Log = {
+        timestamp: string
+        message: string
+        level: LogLevel
+    }
+
+    const maxloglength = 300
 
     const getLogs = () => {
         return JSON.parse(mmkv.getString(Global.Logs) ?? '[]')
@@ -33,5 +55,67 @@ export namespace Logger {
 
     export const flushLogs = () => {
         mmkv.set(Global.Logs, '[]')
+    }
+
+    // new api
+
+    const insertLogs = (data: Log) => {
+        const logs = getLogs()
+        logs.push(data)
+        if (logs.length > maxloglength) logs.shift()
+        mmkv.set(Global.Logs, JSON.stringify(logs))
+    }
+
+    const createLog = (data: string, level: LogLevel): Log => {
+        const timestamp = `[${new Date().toTimeString().substring(0, 8)}]`
+        return { timestamp: timestamp, message: data, level: level }
+    }
+
+    const printLog = (log: Log) => {
+        console.log(`${LevelName[log.level]}${log.timestamp}: ${log.message}`)
+    }
+
+    export const info = (data: string) => {
+        const logItem = createLog(data, LogLevel.INFO)
+        printLog(logItem)
+        insertLogs(logItem)
+    }
+
+    export const infoToast = (data: string) => {
+        info(data)
+        Toast.show(data, toastTime)
+    }
+
+    export const warn = (data: string) => {
+        const logItem = createLog(data, LogLevel.WARN)
+        printLog(logItem)
+        insertLogs(logItem)
+    }
+
+    export const warnToast = (data: string) => {
+        warn(data)
+        Toast.show(data, toastTime)
+    }
+
+    export const error = (data: string) => {
+        const logItem = createLog(data, LogLevel.ERROR)
+        printLog(logItem)
+        insertLogs(logItem)
+    }
+
+    export const errorToast = (data: string) => {
+        error(data)
+        Toast.show(data, toastTime)
+    }
+
+    export const newDebug = (data: string) => {
+        const logItem = createLog(data, LogLevel.DEBUG)
+        printLog(logItem)
+        insertLogs(logItem)
+    }
+
+    export const debugToast = (data: string) => {
+        error(data)
+        Toast.show(data, toastTime)
     }
 }
