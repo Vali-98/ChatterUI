79e8627ef065e53691b8f86581a539f78b3068b8
Vali98 <al244098@gmail.com>
2025-02-02 20:18:29 +0800

    refactor: ported about menu to new system

--- app/About.tsx
+++ app/About.tsx
@@ -1,7 +1,8 @@
 import SupportButton from '@components/buttons/SupportButton'
-import { FontAwesome } from '@expo/vector-icons'
+import ThemedButton from '@components/buttons/ThemedButton'
 import { AppSettings } from '@lib/constants/GlobalValues'
-import { Logger, Style } from '@lib/utils/Global'
+import { Theme } from '@lib/theme/ThemeManager'
+import { Logger } from '@lib/utils/Global'
 import appConfig from 'app.config'
 import { Stack } from 'expo-router'
 import React, { useState } from 'react'
@@ -9,6 +10,8 @@ import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-
 import { useMMKVBoolean } from 'react-native-mmkv'
 
 const About = () => {
+    const styles = useStyles()
+    const { spacing } = Theme.useTheme()
     const [counter, setCounter] = useState<number>(0)
     const [devMode, setDevMode] = useMMKVBoolean(AppSettings.DevMode)
 
@@ -33,76 +36,88 @@ const About = () => {
             <TouchableOpacity activeOpacity={0.8} onPress={updateCounter}>
                 <Image source={require('../assets/images/icon.png')} style={styles.icon} />
             </TouchableOpacity>
+
             <Text style={styles.titleText}>ChatterUI</Text>
             <Text style={styles.subtitleText}>
                 Version {version} {devMode && '[DEV MODE]'}
             </Text>
             {devMode && (
-                <TouchableOpacity
-                    style={{
-                        ...styles.supportButton,
-                        marginTop: 24,
-                        borderColor: Style.getColor('destructive-brand'),
+                <ThemedButton
+                    label="Disabled Dev Mode"
+                    variant="critical"
+                    buttonStyle={{
+                        marginTop: spacing.xl,
                     }}
                     onPress={() => {
                         setCounter(0)
                         setDevMode(false)
                         Logger.log('Dev mode disabled')
-                    }}>
-                    <Text style={styles.supportText}>Disable Dev Mode</Text>
-                </TouchableOpacity>
+                    }}
+                />
             )}
+
             <Text style={styles.body}>
-                ChatterUI is a free and open-source application developed by Vali98
+                ChatterUI is a free and open-source application developed by Vali-98
             </Text>
-            <Text style={{ marginBottom: 20, ...styles.body }}>
+            <Text style={{ marginBottom: spacing.xl3, ...styles.body }}>
                 This app is a passion project I develop in my free time. If you're enjoying the app,
                 consider supporting its development!
             </Text>
-            <Text style={{ ...styles.body, marginBottom: 8 }}>Donate to ChatterUI here:</Text>
-            <SupportButton />
-            <Text style={styles.body}>Got an issue? Report it here:</Text>
+            <Text style={{ ...styles.body, marginBottom: spacing.m }}>
+                Donate to ChatterUI here:
+            </Text>
 
+            <SupportButton />
+
+            <Text style={styles.body}>Got an issue? Report it here:</Text>
             <Text style={styles.subtitleText}>(Don't forget to add your Logs!)</Text>
-            <TouchableOpacity
+
+            <ThemedButton
+                buttonStyle={{ marginTop: spacing.m }}
+                variant="secondary"
+                label="Github Repository"
+                iconName="github"
+                iconSize={20}
                 onPress={() => {
                     Linking.openURL('https://github.com/Vali-98/ChatterUI')
                 }}
-                style={styles.supportButton}>
-                <Text style={styles.supportText}>Github Repository</Text>
-                <FontAwesome name="github" size={20} color={Style.getColor('primary-text1')} />
-            </TouchableOpacity>
+            />
         </View>
     )
 }
 
 export default About
 
-const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 64,
-        paddingVertical: 32,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    titleText: { color: Style.getColor('primary-text1'), fontSize: 32 },
-    subtitleText: { color: Style.getColor('primary-text2') },
-    body: { color: Style.getColor('primary-text1'), marginTop: 20, textAlign: 'center' },
-    icon: {
-        width: 120,
-        height: 120,
-    },
-    supportText: { color: Style.getColor('primary-text2'), paddingRight: 4 },
+const useStyles = () => {
+    const { color, spacing } = Theme.useTheme()
 
-    supportButton: {
-        marginTop: 8,
-        alignSelf: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderColor: Style.getColor('primary-brand'),
-        padding: 8,
-        paddingHorizontal: 16,
-        borderWidth: 1,
-        borderRadius: 16,
-    },
-})
+    return StyleSheet.create({
+        container: {
+            paddingHorizontal: spacing.xl3,
+            paddingBottom: spacing.xl2,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+        },
+        titleText: { color: color.text._100, fontSize: 32 },
+        subtitleText: { color: color.text._400 },
+        body: { color: color.text._100, marginTop: spacing.l, textAlign: 'center' },
+        icon: {
+            width: 120,
+            height: 120,
+        },
+        supportText: { color: color.text._400, paddingRight: spacing.s },
+
+        supportButton: {
+            marginTop: spacing.m,
+            alignSelf: 'center',
+            flexDirection: 'row',
+            alignItems: 'center',
+            borderColor: color.primary._500,
+            padding: spacing.m,
+            paddingHorizontal: spacing.xl,
+            borderWidth: 1,
+            borderRadius: spacing.xl,
+        },
+    })
+}
