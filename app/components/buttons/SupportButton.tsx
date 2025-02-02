edb89881784165b55780dea33e579ee973c930b9
Vali98 <al244098@gmail.com>
2025-02-02 20:17:51 +0800

    refactor: support button to new system

--- app/components/buttons/SupportButton.tsx
+++ app/components/buttons/SupportButton.tsx
@@ -1,30 +1,22 @@
 import { FontAwesome } from '@expo/vector-icons'
 import { Theme } from '@lib/theme/ThemeManager'
 import React from 'react'
-import { Linking, Text, TouchableOpacity } from 'react-native'
+import { Linking } from 'react-native'
+
+import ThemedButton from './ThemedButton'
 
 const SupportButton = () => {
     const theme = Theme.useTheme()
 
     return (
-        <TouchableOpacity
+        <ThemedButton
             onPress={() => {
                 Linking.openURL('https://ko-fi.com/vali98')
             }}
-            style={{
-                alignSelf: 'center',
-                marginBottom: 16,
-                flexDirection: 'row',
-                alignItems: 'center',
-                borderColor: theme.color.primary._500,
-                padding: 8,
-                paddingHorizontal: 16,
-                borderWidth: 1,
-                borderRadius: 16,
-            }}>
-            <Text style={{ color: theme.color.text._400, paddingRight: 4 }}>Support ChatterUI</Text>
-            <FontAwesome name="coffee" size={16} color={theme.color.text._100} />
-        </TouchableOpacity>
+            variant="secondary"
+            label="Support ChatterUI"
+            icon={<FontAwesome name="coffee" size={16} color={theme.color.text._100} />}
+        />
     )
 }
 
