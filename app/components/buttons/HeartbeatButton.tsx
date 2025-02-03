dc81d6674ac7ab02f2d5733f55dcb5aee978c4b0
Vali98 <al244098@gmail.com>
2025-02-03 14:37:59 +0800

    fix: reimplemented heartbeat button

---
+++ app/components/buttons/HeartbeatButton.tsx
@@ -0,0 +1,133 @@
+import { Style } from '@lib/utils/Global'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+const enum ResponseStatus {
+    DEFAULT,
+    OK,
+    ERROR,
+}
+
+type HeartbeatButtonProps = {
+    api: string
+    buttonText?: string
+    apiFormat?: (url: string) => string
+    callback?: () => void
+    messageNeutral?: string
+    messageError?: string
+    messageOK?: string
+    headers?: any
+}
+
+const HeartbeatButton: React.FC<HeartbeatButtonProps> = ({
+    api,
+    buttonText = 'Test',
+    apiFormat = (url: string) => {
+        try {
+            const newurl = new URL('v1/models', api)
+            return newurl.toString()
+        } catch (e) {
+            return ''
+        }
+    },
+    messageNeutral = 'Not Connected',
+    messageError = 'Failed To Connect',
+    messageOK = 'Connected',
+    headers = {},
+    callback = () => {},
+}) => {
+    const [status, setStatus] = useState<ResponseStatus>(ResponseStatus.DEFAULT)
+
+    useEffect(() => {
+        handleCheck()
+    }, [])
+
+    const StatusMessage = () => {
+        switch (status) {
+            case ResponseStatus.DEFAULT:
+                return messageNeutral
+            case ResponseStatus.ERROR:
+                return messageError
+            case ResponseStatus.OK:
+                return messageOK
+        }
+    }
+
+    const handleCheck = async () => {
+        const endpoint = apiFormat(api)
+        try {
+            const controller = new AbortController()
+            const timeout = setTimeout(() => {
+                controller.abort()
+            }, 1000)
+            const response = await fetch(endpoint, {
+                method: 'GET',
+                signal: controller.signal,
+                headers: headers ?? {},
+            }).catch(() => ({ status: 400 }))
+            clearTimeout(timeout)
+            callback()
+            setStatus(response.status === 200 ? ResponseStatus.OK : ResponseStatus.ERROR)
+        } catch (error) {
+            setStatus(ResponseStatus.ERROR)
+        }
+    }
+
+    const getButtonColor = () => {
+        switch (status) {
+            case ResponseStatus.DEFAULT:
+                return Style.getColor('primary-surface1')
+            case ResponseStatus.ERROR:
+                return Style.getColor('destructive-brand')
+            case ResponseStatus.OK:
+                return Style.getColor('confirm-brand')
+        }
+    }
+
+    const buttonColor = getButtonColor()
+
+    return (
+        <View style={{ flexDirection: 'row', marginTop: 8 }}>
+            <TouchableOpacity style={styles.button} onPress={handleCheck}>
+                <Text style={styles.buttonText}>{buttonText}</Text>
+            </TouchableOpacity>
+            <View
+                style={{
+                    marginLeft: 4,
+                    backgroundColor: buttonColor,
+                    borderColor:
+                        status === ResponseStatus.DEFAULT
+                            ? Style.getColor('primary-surface2')
+                            : buttonColor,
+                    padding: 8,
+                    minWidth: 160,
+                    alignItems: 'center',
+                    paddingHorizontal: 16,
+                    borderWidth: 1,
+                    borderRadius: 8,
+                }}>
+                <Text
+                    style={{
+                        color: Style.getColor('primary-text1'),
+                    }}>
+                    {StatusMessage()}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+export default HeartbeatButton
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: Style.getColor('primary-surface4'),
+        padding: 8,
+        paddingHorizontal: 16,
+        borderRadius: 8,
+    },
+
+    buttonText: {
+        color: Style.getColor('primary-text1'),
+    },
+})
