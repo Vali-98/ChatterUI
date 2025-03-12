41c5f29921dce2223aab88f75f119fc1ed072a35
Vali98 <al244098@gmail.com>
2025-03-12 09:48:32 +0800

    feat: updated themed text component

--- app/components/text/TText.tsx
+++ app/components/text/TText.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import { Text, TextProps } from 'react-native'
+import { Theme } from '@lib/theme/ThemeManager'
+import React, { useMemo } from 'react'
+import { Text, TextProps, TextStyle } from 'react-native'
 
-const TText: React.FC<TextProps> = ({ children, ...props }) => {
-    return <Text {...props}>{children}</Text>
+type FontColor = '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
+
+interface TTextProps extends TextProps {
+    color?: FontColor
+}
+
+const TText: React.FC<TTextProps> = ({ color = '500', children, style, ...props }) => {
+    const fontWeightOverride = useMemo(
+        () => ({ fontWeight: (style as TextStyle)?.fontWeight ?? '500' }),
+        [style]
+    )
+
+    const theme = Theme.useTheme()
+    const colorOverride = useMemo(
+        () => ({ color: (style as TextStyle)?.color ?? theme.color.text[`_${color}`] }),
+        [color]
+    )
+    return (
+        <Text style={[style, fontWeightOverride, colorOverride]} {...props}>
+            {children}
+        </Text>
+    )
 }
 
 export default TText
