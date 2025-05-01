1a29887e5acee8ecb67cbdc97ba119c47135954b
Vali98 <al244098@gmail.com>
2025-05-01 20:55:00 +0800

    chore: prettier'd prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-    "trailingComma": "es5",
-    "printWidth": 100,
-    "tabWidth": 4,
-    "singleQuote": true,
-    "bracketSameLine": true,
-    "endOfLine": "crlf",
-    "semi": false
+    trailingComma: 'es5',
+    printWidth: 100,
+    tabWidth: 4,
+    singleQuote: true,
+    bracketSameLine: true,
+    endOfLine: 'crlf',
+    semi: false,
 }
+
