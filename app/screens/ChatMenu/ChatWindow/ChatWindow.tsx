ad66aad9743b5d2642954c20507a3fde411a8416
Vali98 <al244098@gmail.com>
2025-05-30 10:56:39 +0800

    chore: updated cui-llama.rn, reverted legend list usage

--- app/screens/ChatMenu/ChatWindow/ChatWindow.tsx
+++ app/screens/ChatMenu/ChatWindow/ChatWindow.tsx
@@ -4,12 +4,19 @@ import { useBackgroundImage } from '@lib/state/BackgroundImage'
 import { Chats } from '@lib/state/Chat'
 import { AppDirectory } from '@lib/utils/File'
 import { ImageBackground } from 'expo-image'
-import { LegendList } from '@legendapp/list'
+import { FlatList } from 'react-native'
 import { useMMKVBoolean } from 'react-native-mmkv'
 
 import ChatItem from './ChatItem'
 import ChatModelName from './ChatModelName'
 import EditorModal from './EditorModal'
+
+type ListItem = {
+    index: number
+    key: string
+    isLastMessage: boolean
+    isGreeting: boolean
+}
 
 const ChatWindow = () => {
     const { chat } = Chats.useChat()
@@ -19,15 +26,24 @@ const ChatWindow = () => {
 
     const image = useBackgroundImage((state) => state.image)
 
-    if (!chat)
+    const list: ListItem[] = (chat?.messages ?? [])
+        .map((item, index) => ({
+            index: index,
+            key: item.id.toString(),
+            isGreeting: index === 0,
+            isLastMessage: !!chat?.messages && index === chat?.messages.length - 1,
+        }))
+        .reverse()
+
+    const renderItems = ({ item, index }: { item: ListItem; index: number }) => {
         return (
-            <ImageBackground
-                cachePolicy="none"
-                style={{ flex: 1 }}
-                source={{ uri: image ? AppDirectory.Assets + image : '' }}>
-                {showModelname && appMode === 'local' && <ChatModelName />}
-            </ImageBackground>
+            <ChatItem
+                index={item.index}
+                isLastMessage={item.isLastMessage}
+                isGreeting={item.isGreeting}
+            />
         )
+    }
 
     return (
         <ImageBackground
@@ -36,23 +52,15 @@ const ChatWindow = () => {
             source={{ uri: image ? AppDirectory.Assets + image : '' }}>
             <EditorModal />
             {showModelname && appMode === 'local' && <ChatModelName />}
-            <LegendList
-                maintainVisibleContentPosition={true}
+            <FlatList
+                maintainVisibleContentPosition={
+                    autoScroll ? null : { minIndexForVisible: 1, autoscrollToTopThreshold: 50 }
+                }
                 keyboardShouldPersistTaps="handled"
-                alignItemsAtEnd
-                maintainScrollAtEnd
-                estimatedItemSize={400}
-                initialScrollIndex={chat.messages.length}
-                data={chat.messages}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item, index }) => (
-                    <ChatItem
-                        index={index}
-                        isLastMessage={index === chat.messages.length - 1}
-                        isGreeting={index === 0}
-                    />
-                )}
-                extraData={[chat.messages.length]}
+                inverted
+                data={list}
+                keyExtractor={(item) => item.key}
+                renderItem={renderItems}
             />
         </ImageBackground>
     )
