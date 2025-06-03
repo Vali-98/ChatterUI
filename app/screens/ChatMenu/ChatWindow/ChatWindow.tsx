46a1bf17b0ad6b41f9432f71f11e4f765b08e3d9
Vali98 <al244098@gmail.com>
2025-06-03 22:05:05 +0800

    fix: chat window system

--- app/screens/ChatMenu/ChatWindow/ChatWindow.tsx
+++ app/screens/ChatMenu/ChatWindow/ChatWindow.tsx
@@ -4,40 +4,47 @@ import { useBackgroundImage } from '@lib/state/BackgroundImage'
 import { Chats } from '@lib/state/Chat'
 import { AppDirectory } from '@lib/utils/File'
 import { ImageBackground } from 'expo-image'
-import { LegendList, LegendListRef } from '@legendapp/list'
+import { FlatList } from 'react-native'
 import { useMMKVBoolean } from 'react-native-mmkv'
 
 import ChatItem from './ChatItem'
 import ChatModelName from './ChatModelName'
 import EditorModal from './EditorModal'
-import { useEffect, useRef, useState } from 'react'
-import ThemedButton from '@components/buttons/ThemedButton'
-import Animated, { FadeInDown, FadeOutDown } from 'react-native-reanimated'
+
+type ListItem = {
+    index: number
+    key: string
+    isLastMessage: boolean
+    isGreeting: boolean
+}
 
 const ChatWindow = () => {
-    const { chat, chatLength } = Chats.useChat()
+    const { chat } = Chats.useChat()
     const { appMode } = useAppMode()
     const [showModelname, __] = useMMKVBoolean(AppSettings.ShowModelInChat)
     const [autoScroll, ___] = useMMKVBoolean(AppSettings.AutoScroll)
-    const [currentIndex, setCurrentIndex] = useState(0)
+
     const image = useBackgroundImage((state) => state.image)
 
-    if (!chat)
+    const list: ListItem[] = (chat?.messages ?? [])
+        .map((item, index) => ({
+            index: index,
+            key: item.id.toString(),
+            isGreeting: index === 0,
+            isLastMessage: !!chat?.messages && index === chat?.messages.length - 1,
+        }))
+        .reverse()
+
+    const renderItems = ({ item, index }: { item: ListItem; index: number }) => {
         return (
-            <ImageBackground
-                cachePolicy="none"
-                style={{ flex: 1 }}
-                source={{ uri: image ? AppDirectory.Assets + image : '' }}>
-                {showModelname && appMode === 'local' && <ChatModelName />}
-            </ImageBackground>
+            <ChatItem
+                index={item.index}
+                isLastMessage={item.isLastMessage}
+                isGreeting={item.isGreeting}
+            />
         )
+    }
 
-    const listRef = useRef<LegendListRef>(null)
-
-    useEffect(() => {
-        setCurrentIndex(chatLength ?? 0)
-        listRef.current?.scrollToEnd({ animated: false })
-    }, [chat.id])
     return (
         <ImageBackground
             cachePolicy="none"
@@ -45,50 +52,16 @@ const ChatWindow = () => {
             source={{ uri: image ? AppDirectory.Assets + image : '' }}>
             <EditorModal />
             {showModelname && appMode === 'local' && <ChatModelName />}
-            <LegendList
-                ref={listRef}
-                maintainVisibleContentPosition={!autoScroll}
+            <FlatList
+                maintainVisibleContentPosition={
+                    autoScroll ? null : { minIndexForVisible: 1, autoscrollToTopThreshold: 50 }
+                }
                 keyboardShouldPersistTaps="handled"
-                initialContainerPoolRatio={2}
-                waitForInitialLayout
-                alignItemsAtEnd
-                maintainScrollAtEnd
-                maintainScrollAtEndThreshold={0.1}
-                estimatedItemSize={320}
-                initialScrollIndex={chat.messages.length}
-                data={chat.messages}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ index }) => (
-                    <ChatItem
-                        index={index}
-                        isLastMessage={index === chat.messages.length - 1}
-                        isGreeting={index === 0}
-                    />
-                )}
-                extraData={[chat.id, chatLength, chat.messages]}
-                onViewableItemsChanged={({ viewableItems }) => {
-                    if (viewableItems.length > 0) {
-                        const currentIndex = viewableItems[0].index
-                        setCurrentIndex(currentIndex)
-                    }
-                }}
+                inverted
+                data={list}
+                keyExtractor={(item) => item.key}
+                renderItem={renderItems}
             />
-            {chatLength && chatLength - currentIndex > 25 && (
-                <Animated.View entering={FadeInDown.delay(2000)}>
-                    <ThemedButton
-                        label="Jump To Bottom"
-                        buttonStyle={{
-                            position: 'absolute',
-                            bottom: 10,
-                            left: 20,
-                        }}
-                        onPress={() => {
-                            listRef.current?.scrollToEnd({ animated: false })
-                            setCurrentIndex(chatLength)
-                        }}
-                    />
-                </Animated.View>
-            )}
         </ImageBackground>
     )
 }
