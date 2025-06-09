4969b1f76f31f05026538bb046d6950d46355708
Vali98 <al244098@gmail.com>
2025-06-09 23:24:04 +0800

    fix: migration causing loss of foreign key constraints

--- db/migrations/migrations.js
+++ db/migrations/migrations.js
@@ -1,42 +1,37 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
+import journal from './meta/_journal.json'
+import m0000 from './0000_sudden_blockbuster.sql'
+import m0001 from './0001_yellow_squirrel_girl.sql'
+import m0002 from './0002_majestic_brother_voodoo.sql'
+import m0003 from './0003_violet_firelord.sql'
+import m0004 from './0004_small_deadpool.sql'
+import m0005 from './0005_warm_microbe.sql'
+import m0006 from './0006_bored_carnage.sql'
+import m0007 from './0007_loving_magik.sql'
+import m0008 from './0008_fine_sleeper.sql'
+import m0009 from './0009_huge_young_avengers.sql'
+import m0010 from './0010_empty_pandemic.sql'
+import m0011 from './0011_icy_obadiah_stane.sql'
+import m0012 from './0012_tidy_shen.sql'
+import m0013 from './0013_unique_namorita.sql'
+import m0014 from './0014_living_scarlet_spider.sql'
 
-import journal from './meta/_journal.json';
-import m0000 from './0000_sudden_blockbuster.sql';
-import m0001 from './0001_yellow_squirrel_girl.sql';
-import m0002 from './0002_majestic_brother_voodoo.sql';
-import m0003 from './0003_violet_firelord.sql';
-import m0004 from './0004_small_deadpool.sql';
-import m0005 from './0005_warm_microbe.sql';
-import m0006 from './0006_bored_carnage.sql';
-import m0007 from './0007_loving_magik.sql';
-import m0008 from './0008_fine_sleeper.sql';
-import m0009 from './0009_huge_young_avengers.sql';
-import m0010 from './0010_empty_pandemic.sql';
-import m0011 from './0011_icy_obadiah_stane.sql';
-import m0012 from './0012_tidy_shen.sql';
-import m0013 from './0013_unique_namorita.sql';
-import m0014 from './0014_living_scarlet_spider.sql';
-import m0015 from './0015_slimy_retro_girl.sql';
-
-  export default {
+export default {
     journal,
     migrations: {
-      m0000,
-m0001,
-m0002,
-m0003,
-m0004,
-m0005,
-m0006,
-m0007,
-m0008,
-m0009,
-m0010,
-m0011,
-m0012,
-m0013,
-m0014,
-m0015
-    }
-  }
-  
+        m0000,
+        m0001,
+        m0002,
+        m0003,
+        m0004,
+        m0005,
+        m0006,
+        m0007,
+        m0008,
+        m0009,
+        m0010,
+        m0011,
+        m0012,
+        m0013,
+        m0014,
+    },
+}
