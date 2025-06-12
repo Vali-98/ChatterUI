4a297a20c2c6d30df506785aa7834dbb7097eac5
Vali98 <al244098@gmail.com>
2025-06-12 16:26:11 +0800

    feat: added quick new chat button

--- app/screens/ChatMenu/index.tsx
+++ app/screens/ChatMenu/index.tsx
@@ -1,33 +1,33 @@
+import ThemedButton from '@components/buttons/ThemedButton'
 import Drawer from '@components/views/Drawer'
 import HeaderButton from '@components/views/HeaderButton'
 import HeaderTitle from '@components/views/HeaderTitle'
 import { Characters } from '@lib/state/Characters'
 import { Chats } from '@lib/state/Chat'
-import { Theme } from '@lib/theme/ThemeManager'
 import ChatInput from '@screens/ChatMenu/ChatInput'
 import AvatarViewer from '@screens/ChatMenu/ChatWindow/AvatarViewer'
 import ChatWindow from '@screens/ChatMenu/ChatWindow/ChatWindow'
 import ChatsDrawer from '@screens/ChatMenu/ChatsDrawer'
-import OptionsMenu from '@screens/ChatMenu/OptionsMenu'
 import SettingsDrawer from '@screens/SettingsDrawer'
 import { useEffect } from 'react'
 import { SafeAreaView, View } from 'react-native'
 import { useShallow } from 'zustand/react/shallow'
 
 const ChatMenu = () => {
-    const { spacing } = Theme.useTheme()
-    const { unloadCharacter } = Characters.useCharacterCard(
+    const { unloadCharacter, charId } = Characters.useCharacterCard(
         useShallow((state) => ({
             unloadCharacter: state.unloadCard,
+            charId: state.id,
         }))
     )
 
-    const { chat, unloadChat } = Chats.useChat()
+    const { chat, unloadChat, loadChat } = Chats.useChat()
 
-    const { showSettings, showChats } = Drawer.useDrawerState(
+    const { showSettings, showChats, values } = Drawer.useDrawerState(
         useShallow((state) => ({
             showSettings: state.values?.[Drawer.ID.SETTINGS],
             showChats: state.values?.[Drawer.ID.CHATLIST],
+            values: state.values,
         }))
     )
 
@@ -37,6 +37,13 @@ const ChatMenu = () => {
             unloadChat()
         }
     }, [])
+
+    const handleCreateChat = async () => {
+        if (charId)
+            Chats.db.mutate.createChat(charId).then((chatId) => {
+                if (chatId) loadChat(chatId)
+            })
+    }
 
     return (
         <Drawer.Gesture
@@ -54,7 +61,20 @@ const ChatMenu = () => {
                     headerLeft={() => !showChats && <Drawer.Button drawerID={Drawer.ID.SETTINGS} />}
                     headerRight={() =>
                         !showSettings && (
-                            <Drawer.Button drawerID={Drawer.ID.CHATLIST} openIcon="message1" />
+                            <>
+                                {!showChats && (
+                                    <ThemedButton
+                                        buttonStyle={{
+                                            marginRight: 16,
+                                        }}
+                                        iconName="plus"
+                                        variant="tertiary"
+                                        iconSize={24}
+                                        onPress={handleCreateChat}
+                                    />
+                                )}
+                                <Drawer.Button drawerID={Drawer.ID.CHATLIST} openIcon="message1" />
+                            </>
                         )
                     }
                 />
