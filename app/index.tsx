8a963bdd3846e1ff7ec8f8bd8c3b75b773a82256
Vali98 <al244098@gmail.com>
2025-07-22 12:11:32 +0800

    feat: improved error screen

--- app/index.tsx
+++ app/index.tsx
@@ -9,9 +9,10 @@ import CharacterList from '@screens/CharacterListScreen'
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator'
 import { SplashScreen } from 'expo-router'
 import { useEffect, useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import migrations from '../db/migrations/migrations'
+import ThemedButton from '@components/buttons/ThemedButton'
 
 const Home = () => {
     const { color } = Theme.useTheme()
@@ -40,13 +41,30 @@ const Home = () => {
             setFirstRender(false)
             SplashScreen.hideAsync()
         }
-    }, [success])
+        if (error) SplashScreen.hideAsync()
+    }, [success, error])
 
     if (error)
         return (
             <View style={styles.centeredContainer}>
                 <HeaderTitle />
                 <Text style={styles.title}>Database Migration Failed!</Text>
+                <Text style={styles.errorLog}>{error.message}</Text>
+                <Text style={styles.subtitle}>
+                    If you are seeing this, something has gone terribly wrong. Report this error
+                    below, include a screenshot of the log above.
+                </Text>
+                <Text style={styles.subtitle}></Text>
+                <ThemedButton
+                    buttonStyle={{ marginTop: 12 }}
+                    variant="secondary"
+                    label="Github Repository"
+                    iconName="github"
+                    iconSize={20}
+                    onPress={() => {
+                        Linking.openURL('https://github.com/Vali-98/ChatterUI')
+                    }}
+                />
             </View>
         )
 
@@ -84,6 +102,22 @@ const useStyles = () => {
         title: {
             color: color.text._300,
             fontSize: fontSize.xl2,
+        },
+
+        subtitle: {
+            color: color.text._400,
+            marginHorizontal: 32,
+            textAlign: 'center',
+        },
+
+        errorLog: {
+            color: color.text._400,
+            fontSize: fontSize.s,
+            paddingHorizontal: spacing.xl,
+            paddingVertical: spacing.l,
+            borderRadius: 12,
+            margin: spacing.xl2,
+            backgroundColor: 'black',
         },
 
         buttonText: {
