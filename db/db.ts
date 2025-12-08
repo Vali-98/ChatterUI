bd32d3db7af2c083bcd4c86632092d8b0229cb2d
Vali98 <al244098@gmail.com>
2025-12-08 17:04:03 +0800

    feat: readded sqlite-vec managed by expo

--- db/db.ts
+++ db/db.ts
@@ -1,9 +1,11 @@
 import { drizzle } from 'drizzle-orm/expo-sqlite'
-import { openDatabaseSync } from 'expo-sqlite'
+import { openDatabaseSync, bundledExtensions } from 'expo-sqlite'
 
 import * as schema from './schema'
 
 //deleteDatabaseAsync('db.db')
 export const sqliteDB = openDatabaseSync('db.db', { enableChangeListener: true })
+const extension = bundledExtensions['sqlite-vec']
+if (extension) sqliteDB.loadExtensionAsync(extension?.libPath, extension?.entryPoint)
 export const db = drizzle(sqliteDB, { schema })
 sqliteDB.execAsync('PRAGMA foreign_keys = ON;')
