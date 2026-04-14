9e848ebd64c1f35c16053f4d1aceaf730953973d
Vali98 <al244098@gmail.com>
2026-04-14 18:45:32 +0800

    fix: dismiss notifications when swapping into app

--- lib/notifications/Notifications.ts
+++ lib/notifications/Notifications.ts
@@ -1,6 +1,6 @@
 import * as Notifications from 'expo-notifications'
-import { useRouter } from 'expo-router'
-import { useEffect } from 'react'
+import { router } from 'expo-router'
+import { useCallback, useEffect } from 'react'
 import { AppState, Linking, Platform } from 'react-native'
 import { useMMKVBoolean } from 'react-native-mmkv'
 
@@ -28,7 +28,7 @@ export async function registerForPushNotificationsAsync() {
             name: 'chatterUI',
             importance: Notifications.AndroidImportance.DEFAULT,
             vibrationPattern: [250, 0, 250, 250],
-            lightColor: '#FF231F7C',
+            lightColor: '#7d6294',
         })
     }
 
@@ -65,38 +65,47 @@ export function useAppStateNotificationObserver() {
     const [useAuth] = useMMKVBoolean(AppSettings.LocallyAuthenticateUser)
     const { chat, loadChat } = Chats.useChat()
     const { setCard } = Characters.useCharacterStore()
-    const router = useRouter()
 
-    useEffect(() => {
-        const listener = AppState.addEventListener('change', (nextState) => {
-            if (nextState !== 'active') return
-            async function redirect(notification: Notifications.Notification) {
-                if (chat ?? autoLoad ?? useAuth) return
-                const data = notification.request.content.data
-                const chatId = data?.chatId as number | undefined
-                const characterId = data?.characterId as number | undefined
-                if (chatId && characterId) {
-                    Logger.info('Loading chat from notification')
-                    try {
-                        await loadChat(chatId)
-                        await setCard(characterId)
-                        router.navigate('/screens/ChatScreen')
-                        Notifications.clearLastNotificationResponse()
-                    } catch (e) {
-                        Logger.error('Failed to load chat: ' + e)
-                    }
+    const redirect = useCallback(
+        async (notification: Notifications.Notification) => {
+            if (chat ?? autoLoad ?? useAuth) return
+
+            const data = notification.request.content.data
+            const chatId = data?.chatId as number | undefined
+            const characterId = data?.characterId as number | undefined
+
+            if (chatId && characterId) {
+                Logger.info('Loading chat from notification')
+                try {
+                    await loadChat(chatId)
+                    await setCard(characterId)
+                    router.navigate('/screens/ChatScreen')
+                    Notifications.clearLastNotificationResponse()
+                } catch (e) {
+                    Logger.error('Failed to load chat: ' + e)
                 }
             }
+        },
+        [chat, autoLoad, useAuth, loadChat, setCard]
+    )
+
+    useEffect(() => {
+        const listener = AppState.addEventListener('change', async (nextState) => {
+            if (nextState !== 'active') return
 
             const response = Notifications.getLastNotificationResponse()
             if (!response?.notification) {
+                if ((await Notifications.getPresentedNotificationsAsync()).length > 0) {
+                    await Notifications.dismissAllNotificationsAsync()
+                }
                 return
             }
-            redirect(response?.notification)
 
-            return () => {
-                listener.remove()
-            }
+            redirect(response.notification)
         })
-    }, [autoLoad, chat, loadChat, router, setCard, useAuth])
+
+        return () => {
+            listener.remove()
+        }
+    }, [redirect])
 }
