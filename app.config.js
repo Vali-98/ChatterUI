0e2e25aaeca4786acfb452374f954a668025104e
Vali98 <al244098@gmail.com>
2026-04-22 20:57:49 +0800

    chore: updated beta version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = {
         name: IS_DEV ? 'ChatterUI (DEV)' : 'ChatterUI',
         newArchEnabled: true,
         slug: 'ChatterUI',
-        version: '0.8.9-beta10',
+        version: '0.9.0-beta1',
         orientation: 'default',
         icon: './assets/images/icon.png',
         scheme: 'chatterui',
