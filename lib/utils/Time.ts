e0d196ebfdada33f82423a8c72c795b4c9370cee
Vali98 <al244098@gmail.com>
2026-05-10 22:36:24 +0800

    chore: unused function

--- lib/utils/Time.ts
+++ lib/utils/Time.ts
@@ -1,31 +1,3 @@
-export const humanizedISO8601DateTime = (date = '') => {
-    const baseDate = typeof date === 'number' ? new Date(date) : new Date()
-    const humanYear = baseDate.getFullYear()
-    const humanMonth = baseDate.getMonth() + 1
-    const humanDate = baseDate.getDate()
-    const humanHour = (baseDate.getHours() < 10 ? '0' : '') + baseDate.getHours()
-    const humanMinute = (baseDate.getMinutes() < 10 ? '0' : '') + baseDate.getMinutes()
-    const humanSecond = (baseDate.getSeconds() < 10 ? '0' : '') + baseDate.getSeconds()
-    const humanMillisecond =
-        (baseDate.getMilliseconds() < 10 ? '0' : '') + baseDate.getMilliseconds()
-    const HumanizedDateTime =
-        humanYear +
-        '-' +
-        humanMonth +
-        '-' +
-        humanDate +
-        ' @' +
-        humanHour +
-        'h ' +
-        humanMinute +
-        'm ' +
-        humanSecond +
-        's ' +
-        humanMillisecond +
-        'ms'
-    return HumanizedDateTime
-}
-
 const day_ms = 86400000
 export const getFriendlyTimeStamp = (oldtime: number) => {
     const now = Date.now()
