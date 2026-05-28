5766b55782f3a9f6e999115f16f837f8ef96ff60
Vali98 <al244098@gmail.com>
2026-05-28 17:23:42 +0800

    feat: bottom sheet animations

--- app/components/views/BottomSheet.tsx
+++ app/components/views/BottomSheet.tsx
@@ -1,8 +1,9 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import { Modal, View, ViewStyle } from 'react-native'
 import { useReanimatedKeyboardAnimation } from 'react-native-keyboard-controller'
-import Animated, { useAnimatedStyle } from 'react-native-reanimated'
+import Animated, { SlideInDown, SlideOutDown, useAnimatedStyle } from 'react-native-reanimated'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { scheduleOnRN } from 'react-native-worklets'
 
 import { Theme } from '@lib/theme/ThemeManager'
 
@@ -35,6 +36,21 @@ const BottomSheet: React.FC<BottomSheetProps> = ({
             justifyContent: 'flex-end',
         }
     })
+    const [contentVisible, setContentVisible] = useState(false)
+
+    useEffect(() => {
+        setContentVisible(visible)
+    }, [visible, setContentVisible])
+
+    const handleClose = () => {
+        setContentVisible(false)
+    }
+
+    const handleOnClose = () => {
+        setVisible(false)
+        onClose?.()
+    }
+
     return (
         <Modal
             transparent
@@ -42,38 +58,41 @@ const BottomSheet: React.FC<BottomSheetProps> = ({
             navigationBarTranslucent
             onRequestClose={() => {
                 if (onRequestClose) {
-                    onRequestClose(() => setVisible(false))
+                    onRequestClose(handleClose)
                     return
                 }
-                setVisible(false)
-                onClose?.()
+                handleClose()
             }}
             style={{
                 flex: 1,
             }}
             visible={visible}
             animationType="fade">
-            <Animated.View style={[animatedStyle]}>
-                <FadeBackrop handleOverlayClick={() => setVisible(false)} />
-
-                <View
-                    style={[
-                        {
-                            paddingTop: spacing.xl2,
-                            paddingBottom: insets.bottom + spacing.xl2,
-                            paddingLeft: spacing.xl2,
-                            paddingRight: spacing.xl,
-                            maxHeight: '70%',
-                            width: '100%',
-                            borderTopLeftRadius: spacing.xl2,
-                            borderTopRightRadius: spacing.xl2,
-                            backgroundColor: color.neutral._100,
-                        },
-                        sheetStyle,
-                    ]}>
-                    {children}
-                </View>
-            </Animated.View>
+            {contentVisible && (
+                <Animated.View
+                    style={[animatedStyle]}
+                    entering={SlideInDown}
+                    exiting={SlideOutDown.withCallback(() => scheduleOnRN(handleOnClose))}>
+                    <FadeBackrop handleOverlayClick={() => setVisible(false)} />
+                    <View
+                        style={[
+                            {
+                                paddingTop: spacing.xl2,
+                                paddingBottom: insets.bottom + spacing.xl2,
+                                paddingLeft: spacing.xl2,
+                                paddingRight: spacing.xl,
+                                maxHeight: '70%',
+                                width: '100%',
+                                borderTopLeftRadius: spacing.xl2,
+                                borderTopRightRadius: spacing.xl2,
+                                backgroundColor: color.neutral._100,
+                            },
+                            sheetStyle,
+                        ]}>
+                        {children}
+                    </View>
+                </Animated.View>
+            )}
         </Modal>
     )
 }
